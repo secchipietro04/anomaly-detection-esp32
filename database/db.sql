e8d74bf5dae03461648962b1439b84a32fb53c76
secchipietro04 <secchi.pietro04@gmail.com>
2026-08-01 22:50:45 +0200

    updated databse schema- Added foreign key result.datapoint_id->datapoint.id

--- database/db.sql
+++ database/db.sql
@@ -1,44 +1,48 @@
-CREATE TABLE IF NOT EXISTS "sensor" (
-  "id" INT PRIMARY KEY,
-  "settings" VARCHAR(1024)
-);
+CREATE TABLE
+  IF NOT EXISTS "sensor" ("id" INT PRIMARY KEY, "settings" VARCHAR(1024));
 
-CREATE TABLE IF NOT EXISTS "datapoint_schema" (
-  "schema_name" VARCHAR(255) PRIMARY KEY,
-  "description" TEXT,
-  "definition" TEXT NOT NULL
-);
+CREATE TABLE
+  IF NOT EXISTS "datapoint_schema" (
+    "schema_name" VARCHAR(255) PRIMARY KEY,
+    "description" TEXT,
+    "definition" TEXT NOT NULL
+  );
 
-CREATE TABLE IF NOT EXISTS "datapoint" (
-  "id" INT PRIMARY KEY,
-  "from_time" TIMESTAMP,
-  "to_time" TIMESTAMP,
-  "sensor_id" INT NOT NULL,
-  "datapoint_schema" VARCHAR(255) NOT NULL,
-  "data" TEXT
-);
+CREATE TABLE
+  IF NOT EXISTS "datapoint" (
+    "id" INT PRIMARY KEY,
+    "from_time" TIMESTAMP,
+    "to_time" TIMESTAMP,
+    "sensor_id" INT NOT NULL,
+    "datapoint_schema" VARCHAR(255) NOT NULL,
+    "data" TEXT
+  );
 
-CREATE TABLE IF NOT EXISTS "ensemble" (
-  "id" INT PRIMARY KEY,
-  "timestamp_training_date" TIMESTAMP,
-  "type" VARCHAR(100),
-  "sensibility" REAL
-);
+CREATE TABLE
+  IF NOT EXISTS "ensemble" (
+    "id" INT PRIMARY KEY,
+    "timestamp_training_date" TIMESTAMP,
+    "type" VARCHAR(100),
+    "sensibility" REAL
+  );
 
-CREATE TABLE IF NOT EXISTS "model" (
-  "id" INT PRIMARY KEY,
-  "model_data" BYTEA,
-  "info" TEXT
-);
+CREATE TABLE
+  IF NOT EXISTS "model" (
+    "id" INT PRIMARY KEY,
+    "model_data" BYTEA,
+    "info" TEXT
+  );
 
-CREATE TABLE IF NOT EXISTS "result" (
-  "timestamp" TIMESTAMP,
-  "score" NUMERIC,
-  "alert" BOOLEAN,
-  "sensor_id" INT NOT NULL,
-  "model_id" INT NOT NULL,
-  PRIMARY KEY ("sensor_id", "model_id", "timestamp")
-);
+CREATE TABLE
+  IF NOT EXISTS "result" (
+    "timestamp" TIMESTAMP,
+    "score" NUMERIC,
+    "alert" BOOLEAN,
+    "sensor_id" INT NOT NULL,
+    "model_id" INT NOT NULL,
+    "datapoint_id" INT UNIQUE,
+    PRIMARY KEY ("sensor_id", "model_id", "timestamp")
+  );
 
 ALTER TABLE "datapoint" ADD FOREIGN KEY ("sensor_id") REFERENCES "sensor" ("id") ON DELETE CASCADE DEFERRABLE INITIALLY IMMEDIATE;
 
@@ -49,3 +53,5 @@ ALTER TABLE "model" ADD FOREIGN KEY ("id") REFERENCES "ensemble" ("id") ON DELET
 ALTER TABLE "result" ADD FOREIGN KEY ("sensor_id") REFERENCES "sensor" ("id") ON DELETE CASCADE DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "result" ADD FOREIGN KEY ("model_id") REFERENCES "ensemble" ("id") ON DELETE CASCADE DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "result" ADD FOREIGN KEY ("datapoint_id") REFERENCES "datapoint" ("id") ON DELETE SET NULL DEFERRABLE INITIALLY IMMEDIATE;
